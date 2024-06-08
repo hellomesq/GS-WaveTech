d6324139b8bab5f1e67e1060100db6c30c4e66c4
hellomesq <helo.alvz@gmail.com>
2024-06-07 21:00:57 -0300

    modificacao

---
+++ chatbot/scriptChat.js
@@ -0,0 +1,44 @@
+/* MENU */
+class MobileNavbar {
+      constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+        this.handleClick = this.handleClick.bind(this);
+      }
+    
+      animateLinks() {
+        let delay = 0.8; 
+        this.navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${delay}s`;
+                delay += 0.3; 
+            }
+        });
+    }
+      handleClick() {
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+      }
+    
+      addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+      }
+    
+      init() {
+        if (this.mobileMenu) {
+          this.addClickEvent();
+        }
+        return this;
+      }
+    }
+    const mobileNavbar = new MobileNavbar(
+      ".mobile-menu",
+      ".nav-list",
+      ".nav-list li",
+    );
+    mobileNavbar.init();
